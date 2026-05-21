efb14b27909e8d2c3a94a21788b16a1115d2dee3
tondad80-dev <tondad80@gmail.com>
2026-05-21 20:16:28 +0200

    Fix: Add TTY check to conftest.py to prevent EOFError in GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,4 +1,9 @@
-import pytest
+import sys
 
 def pytest_sessionfinish(session, exitstatus):
-    input("\nTesty dokončeny. Stiskněte Enter pro zavření okna...")
+    # Spustí se input() pouze v případě, že je připojen interaktivní terminál (TTY)
+    if sys.stdout.isatty():
+        try:
+            input("\nTesty dokončeny. Stiskněte Enter pro zavření okna...")
+        except EOFError:
+            pass
