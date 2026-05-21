14f2d995629a2ae2a134f4057a75961dab587263
tondad80-dev <tondad80@gmail.com>
2026-05-21 20:20:48 +0200

    Fix: Hardcode CI environment variable check to bypass input

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
-import sys
+import os
 
 def pytest_sessionfinish(session, exitstatus):
-    # Spustí se input() pouze v případě, že je připojen interaktivní terminál (TTY)
-    if sys.stdout.isatty():
+    # Spustí se input() pouze pokud NEJSME v cloudu (GitHub automaticky nastavuje proměnnou CI=true)
+    if not os.getenv("CI"):
         try:
             input("\nTesty dokončeny. Stiskněte Enter pro zavření okna...")
         except EOFError:
